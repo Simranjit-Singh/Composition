71bfe28483b6199664b59808b0c7d000fa14e06c
ssimranj1 <`12345QWERTqwert`12345QWERTqwert>
2020-05-22 21:34:34 -0400

    Initial commit

---
+++ src/TestPerson.java
@@ -0,0 +1,8 @@
+public class TestPerson {
+    public static void main(String[] args) {
+        Person person = new Person();
+        long salary = person.getSalary();
+
+        System.out.println("Salary of person :"+salary);
+    }
+}
